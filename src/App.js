d677e71a187d79c5f05ed35b9d2d18f93df3c78d
patrik.hultgren <info@patrikhultgren.se>
2023-02-04 02:13:47 +0100

    Refactor App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import About from './components/About';
 
-const App = () => (
-  <>
+export default function App() {
+  return <>
     <div className="header-wrapper">
       <Navbar />
       <Intro />
     </div>
     <About />
   </>
-)
+}
 
-
-export default App;
