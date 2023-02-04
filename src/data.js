ad31b910d701801b9bd063782c8febafe7ec493e
patrik.hultgren <info@patrikhultgren.se>
2023-02-04 10:28:22 +0100

    Add Bokus logo

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 import TravrondenIcon from 'primitives/Icon/Travronden'
 import ManufacturingGuideIcon from 'primitives/Icon/ManufacturingGuide'
+import BokusIcon from 'primitives/Icon/Bokus'
 
 export const menuitems = [
   { link: '#om', title: 'Om mig' },
@@ -12,6 +13,12 @@ export const projects = [
     teaser:
       'För Travronden har jag jobbat med ett React baserat CMS som heter TR Content. Systemet används av många sajter inom TR Media.',
     link: 'https://www.travronden.se',
+  },
+  {
+    logo: <BokusIcon />,
+    teaser:
+      'På Bokus var jag med och tog fram den nya kassan för privatpersoner. Den är byggd i React och med Klarna som betalleverantör.',
+    link: 'https://www.bokus.com',
   },
   {
     logo: <ManufacturingGuideIcon />,
