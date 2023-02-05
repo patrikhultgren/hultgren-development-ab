5753673e63c6a0c5458a4435fbdac16a27d23a8f
patrik.hultgren <info@patrikhultgren.se>
2023-02-05 15:22:49 +0100

    Style intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,38 +1,30 @@
 import React from 'react'
+import Container from 'primitives/Container'
 
 const Intro = () => (
-  <article className="intro">
-    <div className="jumbotron">
-      <div className="container">
-        <div className="jumbotron-inner">
-          <h1 className="display-3">
-            Vill du ha hjälp med utvecklingen av din webbplats?
-          </h1>
-          <div className="lead-teaser">
-            <div>
-              <p>Ta gärna kontakt med mig.</p>
-              <p>Patrik Hultgren, frilansande webbutvecklare i Stockholm.</p>
-            </div>
-          </div>
-          <p className="lead-buttons">
-            <a
-              className="btn btn-outline-primary btn-lg ph-mail-link"
-              href="mailto:info@patrikhultgren.se"
-              role="button"
-            >
-              Mejla info@patrikhultgren.se
-            </a>
-            <a
-              className="btn btn-outline-primary btn-lg ph-phone-number ph-phone-number-1"
-              href="tel:0708-445341"
-              role="button"
-            >
-              eller ring 0708-445341
-            </a>
-          </p>
+  <article className="intro text-white">
+    <Container>
+      <div className="text-center max-w-[800px] mx-auto">
+        <h1 className="text-4xl md:text-7xl text-white font-karma font-light text-shadow">
+          Vill du ha hjälp med utvecklingen av din webbplats?
+        </h1>
+        <div className="text-lg mt-4">
+          <p>Ta gärna kontakt med mig.</p>
+          <p>Patrik Hultgren, frilansande webbutvecklare i Stockholm.</p>
+        </div>
+        <div className="mt-12">
+          <a
+            className="border-[3px] px-4 py-3 text-lg"
+            href="mailto:info@patrikhultgren.se"
+          >
+            Mejla info@patrikhultgren.se
+          </a>
+          <a className="border-[3px] px-4 py-3 text-lg" href="tel:0708-445341">
+            eller ring 0708-445341
+          </a>
         </div>
       </div>
-    </div>
+    </Container>
   </article>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      karma: 'Karma, sans-serif, Arial, Verdana',
+    },
   },
   plugins: [],
 }
