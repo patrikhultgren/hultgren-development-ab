097d02c6371d5559a9be9460c6ce22177dbfe6c0
patrik.hultgren <info@patrikhultgren.se>
2023-03-04 23:41:15 +0100

    Correct page title and favicon

--- src/components/Knowledge/index.tsx
+++ src/components/Knowledge/index.tsx
@@ -16,7 +16,7 @@ export default function Knowledge() {
         <ul className="flex text-xl md:text-2xl font-light flex-wrap [&>li]:mt-3 list-none gap-x-8">
           <li>React</li>
           <li>Next.js</li>
-          <li>JavaScript</li>
+          <li>TypeScript</li>
           <li>Tailwind</li>
           <li>Redux-Saga</li>
           <li>Scrum</li>
